4bd7183ff1d10f899056c40a22c4652d0cd6ff51
Peanut58 <1090709@students.wits.ac.za>
2020-03-07 12:14:46 +0200

    Adding edited index, package.json and mainRoutes

--- NPM_Exp/index.js
+++ NPM_Exp/index.js
@@ -19,5 +19,9 @@ const mainRouter = require('./mainRoutes.js')
 //   res.sendFile(path.join(__dirname, 'views', 'about.html'))
 // })
 app.use(mainRouter)
-app.listen(3000)
-console.log('Express server running on port 3000')
+
+const port = process.env.PORT || 3000
+app.listen(port)
+console.log('Express server running on port', port)
+// app.listen(3000)
+// console.log('Express server running on port 3000')
