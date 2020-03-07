5ba39ff656ef276d2278e89ef3ec676344234965
Peanut58 <1090709@students.wits.ac.za>
2020-03-07 11:58:49 +0200

    mainRoutes added and index changed

--- NPM_Exp/index.js
+++ NPM_Exp/index.js
@@ -1,11 +1,23 @@
+
 const path = require('path')
 const express = require('express')
 const app = express()
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
-app.get('/about', function (req, res) {
-  res.sendFile(path.join(__dirname, 'views', 'about.html'))
-})
+const mainRouter = require('./mainRoutes.js')
+// const mainRouter = express.Router()
+
+// mainRouter.get('/', function (req, res) {
+//   res.send('Hello World')
+// })
+// mainRouter.get('/about', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'views', 'about.html'))
+// })
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World')
+// })
+// app.get('/about', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'views', 'about.html'))
+// })
+app.use(mainRouter)
 app.listen(3000)
 console.log('Express server running on port 3000')
