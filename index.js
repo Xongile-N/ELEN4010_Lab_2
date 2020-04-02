7726e841a46522535834b78649fc00316b409ce8
Peanut58 <1090709@students.wits.ac.za>
2020-04-02 12:06:50 +0200

    Lab 3 Server modification

--- index.js
+++ index.js
@@ -1,27 +1,10 @@
-
-const path = require('path')
 const express = require('express')
-const app = express()
-const mainRouter = require('./mainRoutes.js')
-// const mainRouter = express.Router()
-
-// mainRouter.get('/', function (req, res) {
-//   res.send('Hello World')
-// })
-// mainRouter.get('/about', function (req, res) {
-//   res.sendFile(path.join(__dirname, 'views', 'about.html'))
-// })
-
-// app.get('/', function (req, res) {
-//   res.send('Hello World')
-// })
-// app.get('/about', function (req, res) {
-//   res.sendFile(path.join(__dirname, 'views', 'about.html'))
-// })
-app.use(mainRouter)
-
-const port = process.env.PORT || 3000
-app.listen(port)
-console.log('Express server running on port', port)
-// app.listen(3000)
-// console.log('Express server running on port 3000')
+let app = express()
+//loading our routers
+let mainRouter = require('./mainRoutes.js')
+let classRouter = require('./classRoutes.js')
+//mounting our routers
+app.use('/', mainRouter)
+app.use('/class', classRouter)
+app.listen(3000)
+console.log('Express server running on port 3000')
