61abd7b7527d9ba9e22f39d09df2b92f69144b2d
Peanut58 <1090709@students.wits.ac.za>
2020-04-04 10:43:16 +0200

    Trying to implement RESTful Interface

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express')
-let app = express()
-//loading our routers
-let mainRouter = require('./mainRoutes.js')
-let classRouter = require('./classRoutes.js')
-//mounting our routers
+const app = express()
+// loading body-parser
+const bodyParser = require('body-parser')
+// loading our routers
+const mainRouter = require('./mainRoutes.js')
+const todoRouter = require('./classRoutes.js')
+// tell express to use bodyParser for JSON and URL encoded form bodies
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+// mounting our routers
 app.use('/', mainRouter)
-app.use('/class', classRouter)
+app.use('/todo', classRouter)
 app.listen(3000)
 console.log('Express server running on port 3000')
