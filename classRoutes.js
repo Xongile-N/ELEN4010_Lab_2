1d3c9959829b29783cc773bd5d1912f5968f9bdf
Peanut58 <1090709@students.wits.ac.za>
2020-04-06 18:18:23 +0200

    Restful api attempt Lab 3

--- classRoutes.js
+++ classRoutes.js
@@ -1,37 +1,34 @@
+
 const router = express.Router()
-
 const classList = [] // our class list array
-
+router.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'views', 'class', 'index.html'))
+  })
 // RESTful api
 router.get('/api/list', function (req, res) {
   res.json(classList) // Respond with JSON
 })
-
 router.get('/api/get/:id', function (req, res) {
   res.json(classList[req.params.id]) // Notice the wildcard in the URL?
-  // Try browsing to /api/get/0 once you've added some entries
-})
-
-router.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'views', 'class', 'index.html'))
+// Try browsing to /api/get/0 once you've added some entries
 })
 router.post('/api/create', function (req, res) {
   console.log('creating a student entry')
 })
 router.get('/create', function (req, res) {
   res.sendFile(path.join(__dirname, 'views', 'class', 'create.html'))
-})
+  })
 router.post('/api/delete', function (req, res) {
   console.log('deleting a student entry')
 })
-
 router.get('/delete', function (req, res) {
   res.sendFile(path.join(__dirname, 'views', 'class', 'delete.html'))
-})
-router.post('/edit', function (req, res) {
+  })
+  router.post('/edit', function (req, res) {
   res.sendFile(path.join(__dirname, 'views', 'class', 'edit.html'))
-})
+    })
 router.post('/api/edit', function (req, res) {
   console.log('editing a student entry')
 })
+
 module.exports = router
--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const app = express()
 const bodyParser = require('body-parser')
 // loading our routers
 const mainRouter = require('./mainRoutes.js')
-const todoRouter = require('./classRoutes.js')
+const classRouter = require('./classRoutes.js')
 // tell express to use bodyParser for JSON and URL encoded form bodies
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 // mounting our routers
 app.use('/', mainRouter)
-app.use('/todo', classRouter)
-app.listen(3000)
+app.use('/', classRouter)
+
+app.listen(8080)
 console.log('Express server running on port 3000')
