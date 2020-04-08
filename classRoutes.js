cd6a9b44b8d648df3b8d15a3d24fc3010d6e28ba
Peanut58 <1090709@students.wits.ac.za>
2020-04-08 13:29:05 +0200

    Creating Forms for POSTs before Ex5 Lab3

--- classRoutes.js
+++ classRoutes.js
@@ -13,18 +13,28 @@ router.get('/api/get/:id', function (req, res) {
   res.json(classList[req.params.id]) // Notice the wildcard in the URL?
   // Try browsing to /api/get/0 once you've added some entries
 })
+// Create
 router.post('/api/create', function (req, res) {
-  console.log('creating a student entry')
+  console.log('Creating the following student:', req.body.student)
+  classList.push(req.body.student)
+  res.redirect(req.baseUrl + '/api/list')
 })
+// router.post('/api/create', function (req, res) {
+//   console.log('creating a student entry')
+//   res.json('creating')
+// })
+
 router.get('/create', function (req, res) {
   res.sendFile(path.join(__dirname, 'views', 'class', 'create.html'))
 })
+// Delete
 router.post('/api/delete', function (req, res) {
   console.log('deleting a student entry')
 })
 router.get('/delete', function (req, res) {
   res.sendFile(path.join(__dirname, 'views', 'class', 'delete.html'))
 })
+// Edit
 router.post('/edit', function (req, res) {
   res.sendFile(path.join(__dirname, 'views', 'class', 'edit.html'))
 })
